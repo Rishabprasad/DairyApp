32f9afe693480cb1a0895f3b1718058a09d7d572
R3599430 <rishabbabu.prasad@ext.saint-gobain.com>
2019-07-17 21:00:42 +0530

    Added LottieAnimation and changes compile SDK to 28 which doesn't support Android.app.Support

--- app/src/main/java/com/sg/r36a/bharuchdairy/SplashActivity.java
+++ app/src/main/java/com/sg/r36a/bharuchdairy/SplashActivity.java
@@ -1,13 +1,53 @@
 package com.sg.r36a.bharuchdairy;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.blacklist);
+        imageView = findViewById(R.id.logo);
+        TextView appname= findViewById(R.id.appname);
+        appname.setTypeface(typeface);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)this).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)this).getSupportActionBar().show();
     }
 }
